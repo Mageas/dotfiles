da4ff0bc35facd5c7487f960bc1e3ad20e682ecd
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2024-08-14 13:26:52 +0200

    chore: add gnome pinentry

--- gpg/.gnupg/pinentry-switch.sh
+++ gpg/.gnupg/pinentry-switch.sh
@@ -3,5 +3,6 @@
 if [[ "$(uname)" == "Darwin" ]]; then
     /opt/homebrew/bin/pinentry-mac "$@"
 else
-    /usr/bin/pinentry-qt "$@"
+    /usr/bin/pinentry-gnome3 "$@"
+    # /usr/bin/pinentry-qt "$@"
 fi
